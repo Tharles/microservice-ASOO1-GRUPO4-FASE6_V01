a0f26f527eaf0c6485c9926ba56cf5433b20d4c3
Tharles <tdiasdasilva@gmail.com>
2020-03-19 22:35:33 -0300

    Gets and sets fixed

--- src/main/java/com/aso01grupo4fase6/microserviceaso01grupo4fase6/model/Usuario.java
+++ src/main/java/com/aso01grupo4fase6/microserviceaso01grupo4fase6/model/Usuario.java
@@ -11,4 +11,41 @@ public class Usuario {
 	private String nome;
 	private String cpf;
 	
+	public Usuario() {
+		
+	}
+
+	public Usuario(Long id, String nome, String cpf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
 }
